e9563fb1199318d1c4e831bcf9309a5f88970ba9
老鬼 <sukuifly@163.com>
2017-07-21 10:09:42 +0800

    开发环境文件修改自动重启服务

---
+++ FileMonitor.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * 代码来自workerman文件监控重启代码
+ * 建议只允许开发环境使用
+ * workerman(http://www.workerman.net)
+ * @author walkor<walkor@workerman.net>
+ * @link https://github.com/walkor/workerman-filemonitor
+ */
+
+namespace Com\Youzan\ZanHttpDemo\Init\WorkerStart;
+
+use Zan\Framework\Foundation\Core\RunMode;
+use Zan\Framework\Network\Server\Timer\Timer;
+
+class FileMonitor
+{
+    public $monitor_dir;
+    public $last_mtime;
+    public $server;
+    public function bootstrap($server,$workerId)
+    {
+        if(RunMode::isOnline()){
+            return false;
+        }
+        if($workerId == 0){
+            $this->server = $server;
+        $this->monitor_dir = realpath(__DIR__.'/../../');
+        $this->last_mtime = time();
+        Timer::tick(1000,[$this,'check_files_change']);
+        }
+    }
+
+    public function check_files_change(){
+        $dir_iterator = new \RecursiveDirectoryIterator($this->monitor_dir);
+        $iterator = new \RecursiveIteratorIterator($dir_iterator);
+        foreach ($iterator as $file)
+        {
+            if(pathinfo($file, PATHINFO_EXTENSION) != 'php')
+            {
+                continue;
+            }
+            if($this->last_mtime < $file->getMTime())
+            {
+                $this->last_mtime = $file->getMTime();
+                echo $file." update and reload\n";
+                $this->server->swooleServer->reload();
+                break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # zanphp-FileMonitor
-zanphp的开发环境文件修改自动重启代码
+zanphp的开发环境文件修改自动重启代码,原理是通过定时器循环监控和对比目录下PHP文件的最后修改时间,从而判断文件是否更新，是否需求重启服务.
+
+## 安装
+1. 修改FileMonitor.php文件命名空间, 
+```
+<?php
+namespace Com\Youzan\ZanHttpDemo\Init\WorkerStart;
+
+```
+2. 配置项目init
+
+修改项目init/WorkerStart/.config.php (如果没有就新建一个),加入文件监控代码
+
+```
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laogui
+ * Date: 2017/7/20
+ * Time: PM2:17
+ */
+
+return [
+    \Com\Youzan\ZanHttpDemo\Init\WorkerStart\FileMonitor::class
+];
+
+```
+
+## 使用
+安装正常项目启动即可
+
+
+# 注意事项
+本重启机制只适用开发环境方便调试。
+
+尽管代码中判断运行环境。
+但是，严禁在线上生产环境使用.
+严禁在线上生产环境使用.
+
+# 严禁在线上生产环境使用.
